9533808d4b54153d578197892f86235092b75045
Aalian2241 <aalian.2241@gmail.com>
2023-02-04 14:59:13 +0500

    progress on homepage

--- client/screens/HomePage.js
+++ client/screens/HomePage.js
@@ -1,20 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context'
-import tw from "twrnc";
+import { View, Image, TouchableOpacity,Button } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import tw  from "twrnc";
+import { HStack, IconButton } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 const HomePage = () => {
   return (
     <View style={tw`bg-gray-700 flex-1`}>
       <SafeAreaView style={tw`flex-1`}>
-        <View style={tw`w-85 ml-2.5 h-11.5 bg-green-700 rounded-md`}>
+        <View style={tw`h-11.5 bg-green-700 rounded-md`}>
           {/* Your content */}
         </View>
       </SafeAreaView>
+      <View style={tw`bg-white h-12`}>
+      <HStack row center spacing={1} style={tw`justify-around`}>
+        <IconButton style={tw`flex-1 bg-white`} icon={props => <Icon name="book-open-blank-variant" {...props} />} />
+        <IconButton style={tw`flex-1 bg-white`} icon={props => <Icon name="home-outline" {...props} />} />
+        <IconButton style={tw`flex-1 bg-white`} icon={props => <Icon name="clipboard-outline" {...props} />} />
+      </HStack>
+      </View>
     </View>
   );
 };
 
-
-export default HomePage
-
-const styles = StyleSheet.create({})
+export default HomePage;
