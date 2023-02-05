0d9d0ea27c3adb8ff04e7a067b18ecc4c97209ed
Aalian2241 <aalian.2241@gmail.com>
2023-02-05 20:12:25 +0500

    homepage laying out

--- client/screens/HomePage.js
+++ client/screens/HomePage.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { View, Image, TouchableOpacity,Button } from 'react-native';
+import { View, Image, TouchableOpacity} from 'react-native';
 import FastImage from 'react-native-fast-image';
+import {Menu, Divider, Provider, Button } from 'react-native-paper';
 import tw  from "twrnc";
 import { HStack, IconButton } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+
 const HomePage = () => {
+  const [visible1, setVisible1] = React.useState(false);
+  const [visible2, setVisible2] = React.useState(false);
+  const openMenu1 = () => setVisible1(true);
+  const closeMenu1 = () => setVisible1(false);
+
+  const openMenu2 = () => setVisible2(true);
+  const closeMenu2 = () => setVisible2(false);   
+
   return (
-    <View style={tw`bg-gray-700 flex-1`}>
-      <SafeAreaView style={tw`flex-1`}>
-        <View style={tw`h-11.5 bg-green-700 rounded-md`}>
-          {/* Your content */}
+    <Provider>
+      <View style={tw`bg-gray-700 flex-1`}>
+        <SafeAreaView style={tw`flex-1`}>
+          <View style={tw`h-11.5 bg-green-700 rounded-md`} />
+        </SafeAreaView>
+        <View style={tw`bg-white h-12`}>
+          <HStack row center spacing={20} style={tw`justify-around `}>
+            <Menu
+              visible={visible1}
+              onDismiss={closeMenu1}
+              anchor={
+                <Button 
+                  style={tw`w-30`}
+                  onPress={openMenu1}
+                >
+                  Leads
+                </Button>
+              }
+            >
+              <Menu.Item onPress={() => {}} title="Create a Lead" />
+              <Menu.Item onPress={() => {}} title="Manage Leads" />
+              <Divider />
+            </Menu>
+            <IconButton  
+              style={tw`flex-1 bg-white rounded-md`} 
+              icon={props => <Icon name="home-outline" {...props} />} 
+            />
+            <Menu
+              visible={visible2}
+              onDismiss={closeMenu2}
+              anchor={
+                <Button 
+                  style={tw`w-30`}
+                  onPress={openMenu2}
+                >
+                  Quotes
+                </Button>
+              }
+            >
+              <Menu.Item onPress={() => {}} title="Create a Quote" />
+              <Menu.Item onPress={() => {}} title="Manage Quotes" />
+              <Divider />
+            </Menu>
+          </HStack>
         </View>
-      </SafeAreaView>
-      <View style={tw`bg-white h-12`}>
-      <HStack row center spacing={1} style={tw`justify-around`}>
-        <IconButton style={tw`flex-1 bg-white`} icon={props => <Icon name="book-open-blank-variant" {...props} />} />
-        <IconButton style={tw`flex-1 bg-white`} icon={props => <Icon name="home-outline" {...props} />} />
-        <IconButton style={tw`flex-1 bg-white`} icon={props => <Icon name="clipboard-outline" {...props} />} />
-      </HStack>
       </View>
-    </View>
+    </Provider>
   );
 };
 
